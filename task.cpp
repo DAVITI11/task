9ae9e61fc6f05a55167d89e50e049a9cdb13c4bb
DAVITI <datoqajaia10@gmail.com>
2025-05-06 19:17:56 +0400

    first commit

---
+++ task.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+ 
+typedef long long ll;
+ 
+int main() {
+    long long t;cin>>t;
+    cout<<t;
+    return 0;
+}
